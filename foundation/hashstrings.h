f65c02a08dcee5803964023d184de1e4022b3936
Mattias Jansson <mattias@rampantpixels.com>
2013-02-18 20:13:18 +0100

    change engine to foundation

--- foundation/hashstrings.h
+++ foundation/hashstrings.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include <foundation/foundation.h>
@@ -7,7 +6,7 @@
     Edit corresponding definitions file and rerun
     the foundation hashify tool to update this file */
 
-#define HASH_ENGINE static_hash_string( "engine", 0x39c8cc157cfd24f8ULL )
+#define HASH_FOUNDATION static_hash_string( "foundation", 0x13f0d2e482a6eaadULL )
 #define HASH_DEFAULT static_hash_string( "default", 0xe1c3a22ea1763f7bULL )
 #define HASH_NULL static_hash_string( "null", 0x9d633aef96c57587ULL )
 #define HASH_LOCALE static_hash_string( "locale", 0xfb3833132a868037ULL )
