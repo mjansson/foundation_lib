66c0a6703dd762a821e70ce20c49ac9dcd662f9a
Mattias Jansson <mattias@rampantpixels.com>
2013-09-08 21:57:20 +0200

    another apple compilation fix

--- foundation/posix.h
+++ foundation/posix.h
@@ -27,6 +27,9 @@
 #if FOUNDATION_PLATFORM_APPLE
 #define _UUID_T
 #define _UUID_UUID_H
+#define uuid_generate_random __system_uuid_generate_random
+#define uuid_generate_time __system_uuid_generate_time
+#define uuid_is_null __system_uuid_is_null
 #endif
 
 #include <unistd.h>
@@ -46,6 +49,9 @@
 #if FOUNDATION_PLATFORM_APPLE
 #undef _UUID_T
 #undef _UUID_UUID_H
+#undef uuid_generate_random
+#undef uuid_generate_time
+#undef uuid_is_null
 #endif
 
 #endif
