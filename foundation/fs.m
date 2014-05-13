a44a703b5a8d882957613a7edbfe3ac796bcace0
Mattias Jansson <mattias@rampantpixels.com>
2014-05-13 14:02:15 +0200

    reducing memory allocations in the mac file system watcher

--- foundation/fs.m
+++ foundation/fs.m
@@ -28,6 +28,16 @@ void  _fs_event_stream_flush( void* stream );
 //Memory allocation mania should really be cleaned up
 
 
+static void _fs_node_make_path( char* target, const char* first, unsigned int firstlen, const char* second, unsigned int secondlen )
+{
+	memcpy( target, first, firstlen );
+	if( first[firstlen-1] != '/' )
+		target[firstlen++] = '/';
+	memcpy( target + firstlen, second, secondlen );
+	target[firstlen + secondlen] = 0;
+}
+
+
 typedef struct _file_node file_node_t;
 
 struct _file_node
@@ -68,6 +78,7 @@ static void _fs_node_populate( file_node_t* node, const char* fullpath )
 	for( int isub = 0, subsize = array_size( node->files ); isub < subsize; ++isub )
 	{
 		char* filepath = path_merge( fullpath, node->files[isub] );
+		//log_debugf( HASH_FOUNDATION, "  populate found file: %s", filepath );
 		uint64_t last_modified = fs_last_modified( filepath );
 		array_push( node->last_modified, last_modified );
 		string_deallocate( filepath );
@@ -111,40 +122,40 @@ static file_node_t* _fs_node_find( file_node_t* root, const char* path )
 }
 
 
-static void _fs_node_send_deletions( file_node_t* node, const char* path )
+static void _fs_node_send_deletions( file_node_t* node, const char* path, unsigned int pathlen )
 {
+	char pathbuf[FOUNDATION_MAX_PATHLEN+1];
+	
 	for( int ifile = 0, fsize = array_size( node->files ); ifile < fsize; ++ifile )
 	{
-		char* filepath = path_merge( path, node->files[ifile] );
-		//log_infof( HASH_FOUNDATION, "    deleted %s", filepath );
-		fs_post_event( FOUNDATIONEVENT_FILE_DELETED, filepath, 0 );
-		string_deallocate( filepath );
+		_fs_node_make_path( pathbuf, path, pathlen, node->files[ifile], string_length( node->files[ifile] ) );
+		//log_infof( HASH_FOUNDATION, "    subdeleted %s", filepath );
+		fs_post_event( FOUNDATIONEVENT_FILE_DELETED, pathbuf, 0 );
 	}
 
 	for( int isub = 0, subsize = array_size( node->subdirs ); isub < subsize; ++isub )
 	{
-		char* subpath = path_merge( path, node->subdirs[isub]->name );
-		_fs_node_send_deletions( node->subdirs[isub], subpath );
-		string_deallocate( subpath );
+		_fs_node_make_path( pathbuf, path, pathlen, node->subdirs[isub]->name, string_length( node->subdirs[isub]->name ) );
+		_fs_node_send_deletions( node->subdirs[isub], pathbuf, string_length( pathbuf ) );
 	}
 }
 
 
-static void _fs_node_send_creations( file_node_t* node, const char* path )
+static void _fs_node_send_creations( file_node_t* node, const char* path, unsigned int pathlen )
 {
+	char pathbuf[FOUNDATION_MAX_PATHLEN+1];
+	
 	for( int ifile = 0, fsize = array_size( node->files ); ifile < fsize; ++ifile )
 	{
-		char* filepath = path_merge( path, node->files[ifile] );
-		//log_infof( HASH_FOUNDATION, "    created %s", filepath );
-		fs_post_event( FOUNDATIONEVENT_FILE_CREATED, filepath, 0 );
-		string_deallocate( filepath );
+		_fs_node_make_path( pathbuf, path, pathlen, node->files[ifile], string_length( node->files[ifile] ) );
+		//log_infof( HASH_FOUNDATION, "    subcreated %s", filepath );
+		fs_post_event( FOUNDATIONEVENT_FILE_CREATED, pathbuf, 0 );
 	}
 	
 	for( int isub = 0, subsize = array_size( node->subdirs ); isub < subsize; ++isub )
 	{
-		char* subpath = path_merge( path, node->subdirs[isub]->name );
-		_fs_node_send_creations( node->subdirs[isub], subpath );
-		string_deallocate( subpath );
+		_fs_node_make_path( pathbuf, path, pathlen, node->subdirs[isub]->name, string_length( node->subdirs[isub]->name ) );
+		_fs_node_send_creations( node->subdirs[isub], pathbuf, string_length( pathbuf ) );
 	}
 }
 
@@ -153,12 +164,15 @@ static void _fs_event_stream_callback( ConstFSEventStreamRef stream_ref, void* u
 {
 	file_node_t* root_node = user_data;
 	unsigned int root_path_len = string_length( root_node->name );
+	char pathbuf[FOUNDATION_MAX_PATHLEN+1];
 	
 	@autoreleasepool
 	{
 		for( size_t i = 0; i < num_events; ++i )
 		{
-			const char* path = event_paths[i];
+			const char* rawpath = event_paths[i];
+			
+			unsigned int rawpath_len = string_length( rawpath );
             
 			FSEventStreamEventFlags flags = event_flags[i];
 			FSEventStreamEventId identifier = event_ids[i];
@@ -168,34 +182,38 @@ static void _fs_event_stream_callback( ConstFSEventStreamRef stream_ref, void* u
 			if( ( flags & kFSEventStreamEventFlagMustScanSubDirs ) != 0 )
 			{
 				//TODO: Implement
-				log_warnf( 0, WARNING_UNSUPPORTED, "Got kFSEventStreamEventFlagMustScanSubDirs: %s (0x%x 0x%x)", path, (unsigned int)flags, (unsigned int)identifier );
+				log_warnf( 0, WARNING_UNSUPPORTED, "Got kFSEventStreamEventFlagMustScanSubDirs: %s (0x%x 0x%x)", rawpath, (unsigned int)flags, (unsigned int)identifier );
 			}
 			else
 			{
 				//log_debugf( HASH_FOUNDATION, "Got event for: %s (0x%x 0x%x)", path, (unsigned int)flags, (unsigned int)identifier );
 
-				unsigned int root_ofs = string_find_string( path, root_node->name, 0 );
+				unsigned int root_ofs = string_find_string( rawpath, root_node->name, 0 );
 				if( root_ofs == STRING_NPOS )
 					continue;
 				
-				const char* subpath = path + root_ofs + root_path_len + 1;
+				const char* path = rawpath + root_ofs;
+				unsigned int path_len = rawpath_len - root_ofs;
+				
+				const char* subpath = path + root_path_len + 1;
 				
 				file_node_t* node = _fs_node_find( root_node, subpath );
 				if( !node )
 					continue;
 
-				char** files = fs_files( path );
+				char** files = fs_files( rawpath );
 
 				//Check if file have been added, removed or modified
 				for( int isub = 0, subsize = array_size( node->files ); isub < subsize; )
 				{
 					int ifile;
-					char* filepath = path_merge( path + root_ofs, node->files[isub] );
+					
+					_fs_node_make_path( pathbuf, path, path_len, node->files[isub], string_length( node->files[isub] ) );
 					
 					if( ( ifile = string_array_find( (const char* const*)files, node->files[isub], array_size( files ) ) ) == -1 )
 					{
-						//log_debugf( HASH_FOUNDATION, "  deleted: %s", filepath );
-						fs_post_event( FOUNDATIONEVENT_FILE_DELETED, filepath, 0 );
+						//log_debugf( HASH_FOUNDATION, "  deleted: %s", pathbuf );
+						fs_post_event( FOUNDATIONEVENT_FILE_DELETED, pathbuf, 0 );
 						string_deallocate( node->files[isub] );
 						array_erase_memcpy( node->files, isub );
 						array_erase_memcpy( node->last_modified, isub );
@@ -203,36 +221,35 @@ static void _fs_event_stream_callback( ConstFSEventStreamRef stream_ref, void* u
 					}
 					else
 					{
-						uint64_t last_modified = fs_last_modified( filepath );
+						uint64_t last_modified = fs_last_modified( pathbuf );
 						if( last_modified > node->last_modified[ifile] )
 						{
-							//log_debugf( HASH_FOUNDATION, "  modified: %s", filepath );
+							//log_debugf( HASH_FOUNDATION, "  modified: %s", pathbuf );
 							node->last_modified[ifile] = last_modified;
-							fs_post_event( FOUNDATIONEVENT_FILE_MODIFIED, filepath, 0 );
+							fs_post_event( FOUNDATIONEVENT_FILE_MODIFIED, pathbuf, 0 );
 						}
 						++isub;
 					}
-					string_deallocate( filepath );
 				}
 				for( int isub = 0, subsize = array_size( files ); isub < subsize; ++isub )
 				{
 					if( string_array_find( (const char* const*)node->files, files[isub], array_size( node->files ) ) == -1 )
 					{
-						char* filepath = path_merge( path + root_ofs, files[isub] );
-						fs_post_event( FOUNDATIONEVENT_FILE_CREATED, filepath, 0 );
-						//log_debugf( HASH_FOUNDATION, "  created: %s", filepath );
+						_fs_node_make_path( pathbuf, path, path_len, files[isub], string_length( files[isub] ) );
 						
-						array_push( node->last_modified, fs_last_modified( filepath ) );
+						fs_post_event( FOUNDATIONEVENT_FILE_CREATED, pathbuf, 0 );
+						//log_debugf( HASH_FOUNDATION, "  created: %s", pathbuf );
+						
+						array_push( node->last_modified, fs_last_modified( pathbuf ) );
 						array_push( node->files, files[isub] );
 						files[isub] = 0;
-						string_deallocate( filepath );
 					}
 				}
 				
 				string_array_deallocate( files );
 				
 				//Check for subdir additions/removals
-				char** subdirs = fs_subdirs( path );
+				char** subdirs = fs_subdirs( rawpath );
 				for( int iexist = 0, existsize = array_size( node->subdirs ); iexist < existsize; )
 				{
 					bool found = false;
@@ -250,10 +267,8 @@ static void _fs_event_stream_callback( ConstFSEventStreamRef stream_ref, void* u
 						//log_debugf( HASH_FOUNDATION, "  del subdir: %s %s", node->name, node->subdirs[iexist]->name );
 						
 						//Recurse and send out file deletion events
-						char* fullpath = path_merge( path + root_ofs, node->subdirs[iexist]->name );
-						_fs_node_send_deletions( node->subdirs[iexist], fullpath );
-						string_deallocate( fullpath );
-						
+						_fs_node_make_path( pathbuf, rawpath, rawpath_len, node->subdirs[iexist]->name, string_length( node->subdirs[iexist]->name ) );
+						_fs_node_send_deletions( node->subdirs[iexist], pathbuf, string_length( pathbuf ) );
 						_fs_node_deallocate( node->subdirs[iexist] );
 						array_erase_memcpy( node->subdirs, iexist );
 						--existsize;
@@ -281,19 +296,14 @@ static void _fs_event_stream_callback( ConstFSEventStreamRef stream_ref, void* u
 						file_node_t* child = memory_allocate_zero( sizeof( file_node_t ), 0, MEMORY_PERSISTENT );
 						
 						//log_debugf( HASH_FOUNDATION, "  add subdir: %s %s", node->name, subdirs[isub] );
-						
 						child->name = subdirs[isub];
 						subdirs[isub] = 0;
 						
 						array_push( node->subdirs, child );
 						
-						char* fullpath = path_merge( path, child->name );
-						_fs_node_populate( child, fullpath );
-						string_deallocate( fullpath );
-						
-						fullpath = path_merge( path + root_ofs, child->name );
-						_fs_node_send_creations( child, fullpath );
-						string_deallocate( fullpath );
+						_fs_node_make_path( pathbuf, rawpath, rawpath_len, child->name, string_length( child->name ) );
+						_fs_node_populate( child, pathbuf );
+						_fs_node_send_creations( child, pathbuf + root_ofs, string_length( pathbuf + root_ofs ) );
 					}
 				}
 				
