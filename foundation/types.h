a472d8c60bd3e225208d4f20642b17b0edf11ef8
Mattias Jansson <mattias@rampantpixels.com>
2015-01-04 16:16:02 +0000

    fixed indentation

--- foundation/types.h
+++ foundation/types.h
@@ -237,7 +237,7 @@ typedef struct event_block_t                       event_block_t;
 typedef ALIGNED_STRUCT( event_stream_t, 16 )       event_stream_t;
 typedef struct hashmap_node_t                      hashmap_node_t;
 typedef struct hashmap_t                           hashmap_t;
-typedef ALIGNED_STRUCT( hashtable32_entry_t, 8 )  hashtable32_entry_t;
+typedef ALIGNED_STRUCT( hashtable32_entry_t, 8 )   hashtable32_entry_t;
 typedef ALIGNED_STRUCT( hashtable64_entry_t, 8 )   hashtable64_entry_t;
 typedef ALIGNED_STRUCT( hashtable32_t, 8 )         hashtable32_t;
 typedef ALIGNED_STRUCT( hashtable64_t, 8 )         hashtable64_t;
