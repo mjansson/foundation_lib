8bc7ab9fa79ee0e20c455adc64044adae4edf844
Mattias Jansson <mattias@rampantpixels.com>
2015-02-18 09:36:14 +0100

    add version

---
+++ foundation/version.c
@@ -0,0 +1,11 @@
+/* ****** AUTOMATICALLY GENERATED, DO NOT EDIT ******
+   This file is generated from the git describe command.
+   Run the configure script to regeneerate this file */
+
+#include <foundation/platform.h>
+#include <foundation/types.h>
+
+version_t foundation_version( void )
+{
+	return version_make( 1, 2, 4, 0, 0x37bc0c7 );
+}
