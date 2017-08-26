0d36fd4c444a2a83ac889ffcc0967f1f84de0040
Mattias Jansson <mattias@rampantpixels.com>
2017-08-26 19:33:53 +0200

    android compatibility

--- test/all/android/java/com/rampantpixels/foundation/test/TestActivity.java
+++ test/all/android/java/com/rampantpixels/foundation/test/TestActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.view.View;
 import android.view.Gravity;
 import android.view.Display;
 import android.view.ViewGroup;
@@ -20,65 +21,68 @@ public class TestActivity extends NativeActivity
 	private boolean displayedTextView = false;
 
 	@Override
-	public void onWindowFocusChanged( boolean hasFocus )
-	{
-		super.onWindowFocusChanged( hasFocus );
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
 
-		if( !displayedTextView && hasFocus )
-		{
+		if (!displayedTextView && hasFocus) {
 			displayedTextView = true;
 
-			setContentView( R.layout.main );
+			setContentView(R.layout.main);
 
-			textView = (TextView)findViewById( R.id.logtext );
-			textView.setText( "" );
+			textView = (TextView)findViewById(R.id.logtext);
+			textView.setText("");
+			textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {}
+            });
+            textView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) { return true; }
+            });
 			((ViewGroup)textView.getParent()).removeView(textView);
 
 			final TestActivity activity = this;
 
-			runOnUiThread( new Runnable() {
-
+			runOnUiThread(new Runnable() {
 				@Override
-				public void run()
-				{
-					PopupWindow popup = new PopupWindow( activity );
+				public void run() {
+					PopupWindow popup = new PopupWindow(activity);
 
 					Display display = getWindowManager().getDefaultDisplay();
 					Point size = new Point();
-					display.getSize( size );
+					display.getSize(size);
 
-					popup.setWidth( size.x );
-					popup.setHeight( size.y );
-					popup.setWindowLayoutMode( LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT );
-					popup.setClippingEnabled( false );
+					popup.setWidth(size.x);
+					popup.setHeight(size.y);
+					popup.setWindowLayoutMode(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+					popup.setClippingEnabled(false);
 
-					MarginLayoutParams params = new MarginLayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT );
+					MarginLayoutParams params = new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 					params.setMargins(0, 0, 0, 0);
 
-					LinearLayout layout = new LinearLayout( activity );
-					layout.setOrientation( LinearLayout.VERTICAL );
-					layout.addView( activity.textView, params );
+					LinearLayout layout = new LinearLayout(activity);
+					layout.setOrientation(LinearLayout.VERTICAL);
+					layout.addView(activity.textView, params);
 
-					popup.setContentView( layout );
+					popup.setContentView(layout);
 
-					final ViewGroup viewGroup = (ViewGroup)((ViewGroup)activity.findViewById( android.R.id.content )).getChildAt(0);
+					final ViewGroup viewGroup = (ViewGroup)((ViewGroup)activity.findViewById(android.R.id.content)).getChildAt(0);
 
-					popup.showAtLocation( viewGroup, Gravity.TOP, 0, 0 );
+					popup.showAtLocation(viewGroup, Gravity.TOP, 0, 0);
 					popup.update();
 				}
-			} );
+			});
 		}
 	}
 
 	public void appendLog( final String msg )
 	{
-		runOnUiThread( new Runnable() {
+		runOnUiThread(new Runnable() {
 			@Override
-			public void run()
-			{
-				if( textView != null )
-					textView.append( msg );
+			public void run() {
+				if (textView != null)
+					textView.append(msg);
 			}
-		} );
+		});
 	}
 }
