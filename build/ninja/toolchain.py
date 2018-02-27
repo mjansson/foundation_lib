2d2c13777ac5911e437cae2fe636aa9942e0fed5
Mattias Jansson <mattias@rampantpixels.com>
2018-02-27 21:00:39 +0000

    restore lost byte decode

--- build/ninja/toolchain.py
+++ build/ninja/toolchain.py
@@ -133,7 +133,7 @@ class Toolchain(object):
     if self.target.is_windows():
       self.archs = ['x86-64']
     elif self.target.is_linux() or self.target.is_bsd():
-      localarch = subprocess.check_output(['uname', '-m']).strip()
+      localarch = subprocess.check_output(['uname', '-m']).decode().strip()
       if localarch == 'x86_64' or localarch == 'amd64':
         self.archs = ['x86-64']
       elif localarch == 'i686':
